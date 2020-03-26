eeb060f79cca564f76323dba410dcbf5672de76c
han95210 <han95210@naver.com>
2020-03-26 20:28:43 +0900

    스프링 부트 개발환경 세팅 및 테스트 페이지 생성

---
+++ src/test/java/com/codesquad/signup12/Signup12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.signup12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Signup12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
